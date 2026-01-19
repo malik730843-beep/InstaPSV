57bf4d8247b4c7b1f1f55cf88c8cb0b4b70d0cac
User <user@example.com>
2026-01-19 10:55:08 +0500

    Add Microsoft Clarity integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -246,7 +246,18 @@ export default async function RootLayout({
             </Script>
           </>
         )}
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v3qtewuzks");
+          `}
+        </Script>
       </body>
-    </html>
+    </html >
   );
 }
