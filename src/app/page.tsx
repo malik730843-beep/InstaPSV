28c8cc331a7d7f00ab2b6c9b0bf203559b99213e
User <user@example.com>
2026-04-25 19:43:29 +0500

    Remove all ads from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import ComparisonTable from '@/components/sections/ComparisonTable';
 import CTASection from '@/components/sections/CTASection';
 import FeatureSuggestion from '@/components/sections/FeatureSuggestion';
 import FAQSchema from '@/components/seo/FAQSchema';
-import AdsterraBanner from '@/components/ads/AdsterraBanner';
 import dynamic from 'next/dynamic';
 
 const Testimonials = dynamic(() => import('@/components/sections/Testimonials'), {
@@ -25,21 +24,13 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        <AdsterraBanner />
         <SecurityComparison />
-        <AdsterraBanner />
         <Features />
-        <AdsterraBanner />
         <HowItWorks />
-        <AdsterraBanner />
         <ComparisonTable />
-        <AdsterraBanner />
         <UseCases />
-        <AdsterraBanner />
         <FAQ />
-        <AdsterraBanner />
         <Testimonials />
-        <AdsterraBanner />
         <FeatureSuggestion />
       </main>
       <Footer />
